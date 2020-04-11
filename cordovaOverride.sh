323535abb2674348b6a19ec6fe95b2bbaf094abd
mustapha <mofodev01@gmail.com>
2020-04-11 17:43:39 +0100

    reles-v0.2.1

--- cordovaOverride.sh
+++ cordovaOverride.sh
@@ -7,5 +7,6 @@ echo "In Appflow...using cordova@8.1.2"
 npm uninstall -g @ionic-enterprise/cordova
 npm install -g cordova@8.1.2
 echo "In Appflow...using cordova-ios"
-npm i cordova-ios@latest
+#npm i cordova-ios@latest
+npm i cordova-ios@6.0.0-nightly.2020.4.11.449b9ccd
 fi
